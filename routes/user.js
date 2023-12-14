07c1323e95bafe4b9b50058995eed465daf92c1d
mah-pro <142360247+mah-pro@users.noreply.github.com>
2023-12-14 17:48:02 +0000

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,15 @@
 // Importation du module Router d'Express
 const router = require("express").Router();
-
 // Importation de la fonction catchErrors depuis le gestionnaire d'erreurs
 const { catchErrors } = require("../handlers/errorHandlers");
-
 // Importation du contrôleur des utilisateurs
 const userController = require("../controllers/userController");
 
 // Définition de la route pour la connexion d'un utilisateur
-router.post("http://localhost:8000/user/login", catchErrors(userController.login));
+router.post("/login", catchErrors(userController.login));
 
 // Définition de la route pour l'inscription d'un utilisateur
-router.post("http://localhost:8000/user/register", catchErrors(userController.register));
+router.post("/register", catchErrors(userController.register));
 
 // Exportation du routeur configuré
 module.exports = router;
